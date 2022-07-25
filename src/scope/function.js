161d4385a4e5ed85c452d450078f17a147b46355
arbey poveda <arpoved@gmail.com>
2022-07-25 09:06:45 -0500

    agregue dos carpetas al curso

---
+++ src/scope/function.js
@@ -0,0 +1,13 @@
+function greeting(){
+    let userName = "camila";
+    console.log(useName);
+
+    if(useName === "angie"){
+        console.log(`hello ${userName}`)
+    }else
+        console.log("quien es usted socia")
+    
+}
+
+greeting();
+console.log(userName);
