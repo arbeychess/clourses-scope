a2ae9613aaa41483f3b4b4da1ca6fc4d1c4f6c4b
arbey poveda <arpoved@gmail.com>
2022-07-26 07:56:19 -0500

    re agregue

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+var firtsName; //undefined
+firtsName = "arbey";
+console.log(firtsName);
+
+var lastName = "nelson"; // declarar /asignar
+lastName = "carlos"; // reasignar
+console.log(lastName);
+
+
+var secondName = "angie";  // declarando // asignando
+var secondName = "ana"; // reasignando
+console.log(secondName);
+
+
+//con let // no se puede redeclarar solo reasignar
+
+let fruit = "apple" //declarar y asignar
+fruit = "kiwi" //reasignar
+console.log(fruit);
+
+// con const no se deja reasignar ni redeclarar
+
+const animal = "dog"; // declaro y asigno
+console.log(animal);
+
+
+
+
+
+
+
+
+
